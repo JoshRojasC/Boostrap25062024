3733901157b708c101ea48d8c1044d7e8cbb261c
JoshRojasC <josurojascruz@gmail.com>
2024-07-02 20:08:23 -0600

    Feat-trabajo en clase JS

---
+++ Js/Variables.js
@@ -0,0 +1,11 @@
+console.log("Hola Mundo");
+
+let nombre="Josue"
+let apellido="Rojas"
+let edad="28"
+let direccion="Puntarenas"
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(direccion)
