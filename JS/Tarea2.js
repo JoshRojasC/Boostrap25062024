67e6fcd477aaec37868e96d15662baebf08f42a8
JoshRojasC <josurojascruz@gmail.com>
2024-07-17 10:18:50 -0600

    avance de la tarea

---
+++ JS/Tarea2.js
@@ -0,0 +1,11 @@
+function formularioSolicitud = (event){
+    event.preventdefault();
+
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    let asunto = document.getElementById("asunto").value;
+    let mensaje = document.getElementById("mensaje").value;
+    let copia = document.getElementById("copia").value;
+
+
+}
